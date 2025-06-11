256418aec2f8541ddbee8715fc67d62e3aef937d
Madrajib LAB <madhabsharma94@gmail.com>
2025-06-11 17:42:05 +0530

    Add Elf Section Parsers

---
+++ ElfSectionParsers.cs
@@ -0,0 +1,81 @@
+using ELFSharp.ELF;
+using System.Text;
+
+namespace DwarfParser
+{
+    public class DebugStr
+    {
+        private byte[] dataBytes;
+        public DebugStr(IELF elfFile)
+        {
+            dataBytes = elfFile.Sections.Where(s => s.Name == ".debug_str").First().GetContents();
+        }
+
+        public String readStrFrom(UInt64 offset)
+        {
+            var output = new List<byte>();
+            byte character;
+            while ((character = dataBytes[offset]) > 0)
+            {
+                output.Add(character);
+                offset++;
+            }
+            return Encoding.ASCII.GetString(output.ToArray());
+        }
+    }
+
+    public class DebugStrOff
+    {
+        private byte[] dataBytes;
+        private ulong index = 0;
+        private byte unit_length_bytes_count;
+        private bool is64bitDW = false;
+        UInt16 version;
+        UInt64 unit_length;
+        UInt16 padding = 0;
+        public DebugStrOff(IELF elfFile)
+        {
+            dataBytes = elfFile.Sections.Where(s => s.Name == ".debug_str_offsets").First().GetContents();
+            parseHeader();
+        }
+
+        private void parseHeader()
+        {
+            unit_length = BitConverter.ToUInt32(dataBytes, (int)index);
+            index += 4;
+
+            if (unit_length == 0xffffffff)
+            {
+                is64bitDW = true;
+                unit_length = BitConverter.ToUInt64(dataBytes, (int)index);
+                index += 8;
+                unit_length_bytes_count = 12;
+            }
+            else
+            {
+                is64bitDW = false;
+                unit_length_bytes_count = 4;
+            }
+
+            version = BitConverter.ToUInt16(dataBytes, (int)index);
+            index += 2;
+
+            padding = BitConverter.ToUInt16(dataBytes, (int)index);
+            index += 2;
+        }
+
+        public UInt64 readOffsetFrom(UInt64 offset)
+        {
+            if (is64bitDW)
+                return BitConverter.ToUInt64(dataBytes, (int)(index + offset));
+            else
+                return BitConverter.ToUInt32(dataBytes, (int)(index + offset));
+        }
+
+        public override string ToString()
+        {
+            return $" unit_len {unit_length} ver {version}  padd: {padding}";
+        }
+    }
+
+}
